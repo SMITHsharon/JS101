c4a8dc982f98ba6e08c4b4c7530a8aa491fe70e6
sharon smith <sharon.e.smith2@gmail.com>
2017-01-23 21:42:07 -0600

    dropped sonnet text in html; grab text in js

---
+++ sonnet.js
@@ -0,0 +1,21 @@
+// Paste the code below into your HTML file.
+
+// Take the contents of the sonnet div and place it in a variable
+var element = document.getElementById("sonnet"); 
+// .innerHTML ; wrap this part to next line => assign
+
+var sonnet = element.innerHTML;
+
+console.log(sonnet);
+
+// Find and output the starting position of the word "orphans"
+var string = "orphans";
+var startIndex;
+console.log("Start position of orphans = ", startIndex);
+
+// Output the number of characters in the sonnet
+// Replace the first occurance of the string "winter" with "yuletide"
+// Replace all occurances of the string "the" with "a large"
+// Set the content of the sonnet div with the new string
+
+
