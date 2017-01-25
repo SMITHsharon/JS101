3322350a7c1d806f87b2fe350231db198b9e763c
sharon smith <sharon.e.smith2@gmail.com>
2017-01-24 19:24:26 -0600

    added switch statement to js file

---
+++ day2.js
@@ -0,0 +1,11 @@
+// switch statement
+var day = "Friday";
+
+switch(day) {
+	case "Monday": 
+		console.log("Booooooo");
+	case "Friday":
+		console.log("PARTAY");
+	default: 
+		console.log("not monday");
+}
