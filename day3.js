94abce682fa51f61e5214dd2a07143a10bc5615d
sharon smith <sharon.e.smith2@gmail.com>
2017-03-26 19:37:03 -0500

    commiting changes

--- day3.js
+++ day3.js
@@ -17,3 +17,63 @@ for (var myKey in myCat) {
 	console.log("values", myCat[myKey]);
 }
 
+
+
+// FUNCTIONS
+var nuggetElement = document.getElementById("nuggetShack");
+var availElement = document.getElementById("availForPurchase");
+console.log("nuggetElement ::", nuggetElement);
+console.log("availElement ::", availElement);
+
+function nuggetMaker(animal) {
+	var myNugget = "breading " + animal + " breading ";
+	return myNugget;
+}
+
+function addBr(){
+	return "<br>"
+}
+
+function shackMaker (divLocation, divText) {
+	var newBreak = addBr();
+	divLocation.innerHTML += divText + newBreak;
+}
+
+
+
+var chickenNuggets = nuggetMaker("chicken");
+// console.log("chickentNuggets :: ", chickenNuggets);
+// nuggetElement.innerHTML = chickenNuggets;
+shackMaker(availElement, chickenNuggets);
+
+var lobsterNuggets = nuggetMaker("lobster");
+// console.log("lobsterNuggets :: ", lobsterNuggets);
+// nuggetElement.innerHTML += lobsterNuggets;
+shackMaker(availElement, lobsterNuggets);
+
+var tablets = nuggetMaker("myComputer");
+// console.log("tabletNuggets :: ", tablets);
+// nuggetElement.innerHTML += tablets;
+shackMaker(availElement, tablets);
+
+var shackName = "Callan's Shack";
+// nuggetElement.innerHTML += shackName;
+shackMaker(nuggetElement, shackName);
+
+
+
+function myLooper(myArray) {
+	for (var i=0; i<myArray.length; i++) {
+		console.log(myArray[i]);
+	}
+}
+
+
+var animals = ["cats", "turtles", "fish", "bears", "tigers"]; 
+myLooper(animals);
+
+var colors = ["red", "blue", "gold", "pink", "orange"]; 
+myLooper(colors);
+
+
+
